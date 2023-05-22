eaef6c0a5189fdf39f38c2b3823ce7f5990a69f7
Rafael Sanchez <rafael.sanchez.aguilu@gmail.com>
2023-05-22 09:07:03 -0400

    update

--- init.lua
+++ init.lua
@@ -30,17 +30,20 @@ vim.api.nvim_create_autocmd({"ColorScheme"}, {
 
 vim.o.guifont = table.concat({ "Consolas:h10", "Courier_New:h10" }, ",")
 
-vim.api.nvim_create_autocmd({"VimEnter"}, {
-  pattern = {"*"},
-  callback = function() 
-    vim.cmd("colorscheme " .. vim.g.COLOR)
-  end
-})
+vim.api.nvim_create_autocmd(
+  {"VimEnter"}, {
+    pattern = {"*"},
+    callback = function() 
+      vim.cmd("colorscheme " .. vim.g.COLOR)
+    end
+  })
 
-vim.api.nvim_create_autocmd({"FileType"}, {
+vim.api.nvim_create_autocmd(
+  {"FileType"}, {
     pattern = "vim,lua",
     callback = function(ev)
       vim.opt.expandtab = true
+      vim.opt.keywordprg = ':help'
       vim.opt.shiftwidth = 2
       vim.opt.tabstop = 2
     end
@@ -48,130 +51,39 @@ vim.api.nvim_create_autocmd({"FileType"}, {
 
 local packs = {}
 
+-- plugins
 table.insert(packs, "BurntSushi/ripgrep")
-table.insert(packs, "nvim-lua/plenary.nvim") -- library for many plugins
-table.insert(packs, "sbdchd/neoformat")
-table.insert(packs, "stevearc/dressing.nvim") -- Make UI prettier
-table.insert(packs, { "ellisonleao/glow.nvim", config = true, cmd = "Glow" }) -- Preview markdown documents.
 table.insert(packs, "LinArcX/telescope-command-palette.nvim")
+table.insert(packs, "bakpakin/janet.vim")
+table.insert(packs, "tpope/vim-unimpaired")
+table.insert(packs, { "folke/which-key.nvim", run = function() require("which-key").setup({ }) end })
 
-table.insert(packs, { 
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.1",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require('telescope').setup({
-			extensions = {
-				command_palette = {
-					{
-						"File",
-						{ "entire selection (C-a)", ':call feedkeys("GVgg")' },
-						{ "save current file (C-s)", ':w' },
-						{ "save all files (C-A-s)", ':wa' },
-						{ "quit (C-q)", ':qa' },
-						{ "file browser (C-i)", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
-						{ "search word (A-w)", ":lua require('telescope.builtin').live_grep()", 1 },
-						{ "git files (A-f)", ":lua require('telescope.builtin').git_files()", 1 },
-						{ "files (C-f)",     ":lua require('telescope.builtin').find_files()", 1 },
-					},
-					{
-						"Help",
-						{ "tips", ":help tips" },
-						{ "cheatsheet", ":help index" },
-						{ "tutorial", ":help tutor" },
-						{ "summary", ":help summary" },
-						{ "quick reference", ":help quickref" },
-						{ "search help(F1)", ":lua require('telescope.builtin').help_tags()", 1 },
-					},
-					{	
-						"Vim",
-						{ "reload vimrc", ":source $MYVIMRC" },
-						{ 'check health', ":checkhealth" },
-						{ "jumps (Alt-j)", ":lua require('telescope.builtin').jumplist()" },
-						{ "commands", ":lua require('telescope.builtin').commands()" },
-						{ "command history", ":lua require('telescope.builtin').command_history()" },
-						{ "registers (A-e)", ":lua require('telescope.builtin').registers()" },
-						{ "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
-						{ "vim options", ":lua require('telescope.builtin').vim_options()" },
-						{ "keymaps", ":lua require('telescope.builtin').keymaps()" },
-						{ "buffers", ":Telescope buffers" },
-						{ "search history (C-h)", ":lua require('telescope.builtin').search_history()" },
-						{ "paste mode", ':set paste!' },
-						{ 'cursor line', ':set cursorline!' },
-						{ 'cursor column', ':set cursorcolumn!' },
-						{ "spell checker", ':set spell!' },
-						{ "relative number", ':set relativenumber!' },
-						{ "search highlighting (F12)", ':set hlsearch!' },
-					}
-				}
-			}
-		})
-    require('telescope').load_extension("command_palette")
-    vim.keymap.set("n", "<F12>", ":Telescope command_palette<cr>")
-	end
-})
+-- themes
+table.insert(packs, "NLKNguyen/papercolor-theme")
+table.insert(packs, "arcticicestudio/nord-vim")
+table.insert(packs, "cocopon/iceberg.vim")
+table.insert(packs, "preservim/vim-colors-pencil")
+table.insert(packs, "ray-x/aurora")
+table.insert(packs, "rebelot/kanagawa.nvim")
+table.insert(packs, "sts10/vim-pink-moon")
+table.insert(packs, "wadackel/vim-dogrun")
+table.insert(packs, { "Mofiqul/dracula.nvim", as = "dracula" })
+table.insert(packs, { "catppuccin/nvim", as = "catppuccin" })
+table.insert(packs, { "challenger-deep-theme/vim", as = "challenger-deep-theme" })
+table.insert(packs, { "embark-theme/vim", as = "embark-theme" })
+table.insert(packs, "archseer/colibri.vim")
 
-table.insert(packs, {
-	"dcampos/nvim-snippy", 
-	config = function()
-		require("snippy").setup({
-			mappings = {
-				is = {
-					["<Tab>"] = "expand_or_advance",
-					["<S-Tab>"] = "previous",
-				},
-				nx = {
-					["<leader>x"] = "cut_text",
-				},
-			},
-		})
-	end,
-})
-
-table.insert(packs, {
-	"folke/which-key.nvim",
-	config = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-    require("which-key").setup({ })
-	end,
-})
-
-local themes = {
-  "archseer/colibri.vim", -- Color similar to helix text editor
-  "NLKNguyen/papercolor-theme",
-  {"catppuccin/nvim", name = "catppuccin", as = "catppuccin" },
-  {"Mofiqul/dracula.nvim", name = "dracula.nvim", as = "dracula"},
-  "cocopon/iceberg.vim",
-  "wadackel/vim-dogrun",
-  "ray-x/aurora",
-  "preservim/vim-colors-pencil",
-  "arcticicestudio/nord-vim",
-  {"embark-theme/vim", name = "embark-theme", as = "embark-theme" },
-  {"challenger-deep-theme/vim", name = "challenger-deep-theme", as = "challenger-deep-theme" },
-}
-for k, v in pairs(themes) do table.insert(packs, v) end
-
-local paq_on, paq = pcall(require, "paq")
-if false and paq_on then
+-- paq https://github.com/savq/paq-nvim
+local function init_paq()
+  local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+  local installed = vim.fn.empty(vim.fn.glob(path)) == 0
+  if not installed then
+    return vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+  end
+  local paq_on, paq = pcall(require, "paq")
   table.insert(packs, 1, "savq/paq-nvim")
   paq { packs }
 end
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-local lazy_on, lazy = pcall(require, "lazy")
-if true and lazy_on then lazy.setup(packs) end
 
 if vim.g.goneovim then
   -- https://github.com/akiyosi/goneovim
