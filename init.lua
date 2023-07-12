526a67e5b811a6eb1d1d84695c6af1fa899d3b82
Rafael Sanchez <rafael.sanchez.aguilu@gmail.com>
2023-07-12 09:17:02 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,43 +4,41 @@
 --   Unix: "~/.config/nvim/plugin/local.vim"
 
 -- https://neovim.io/doc/user/options.html#option-summary
-vim.opt.browsedir = "buffer"
+vim.opt.browsedir = 'buffer'
 vim.opt.cursorline = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.wildmode = "longest:full"
-
-vim.g.mapleader = " "
+vim.opt.wildmode = 'longest:full'
 
 -- Cycle through my preferred colors
-local colors = { "catppuccin", "colibri", "nord", "iceberg", "aurora" }
+local colors = { 'catppuccin', 'colibri', 'nord', 'iceberg', 'aurora' }
 color_index = #colors
-vim.keymap.set("", "<C-F11>", 
+vim.keymap.set('', '<C-F11>', 
   function()
     color_index = color_index + 1
     if color_index > #colors then color_index = 1 end
-    vim.cmd("colorscheme " .. colors[color_index])
-    vim.cmd("echo g:colors_name")
+    vim.cmd('colorscheme ' .. colors[color_index])
+    vim.cmd('echo g:colors_name')
   end)
 
-vim.api.nvim_create_autocmd({"ColorScheme"}, {
-  pattern = {"*"},
-  command = "let COLOR = g:colors_name"
+vim.api.nvim_create_autocmd({'ColorScheme'}, {
+  pattern = {'*'},
+  command = 'let COLOR = g:colors_name'
 })
 
-vim.o.guifont = table.concat({ "Consolas:h10", "Courier_New:h10" }, ",")
+vim.o.guifont = table.concat({ 'Consolas:h10', 'Courier_New:h10' }, ',')
 
 vim.api.nvim_create_autocmd(
-  {"VimEnter"}, {
-    pattern = {"*"},
+  {'VimEnter'}, {
+    pattern = {'*'},
     callback = function() 
-      vim.cmd("colorscheme " .. vim.g.COLOR)
+      vim.cmd('colorscheme ' .. vim.g.COLOR)
     end
   })
 
 vim.api.nvim_create_autocmd(
-  {"FileType"}, {
-    pattern = "vim,lua",
+  {'FileType'}, {
+    pattern = 'vim,lua',
     callback = function(ev)
       vim.opt.expandtab = true
       vim.opt.keywordprg = ':help'
@@ -49,50 +47,8 @@ vim.api.nvim_create_autocmd(
     end
   })
 
--- paq https://github.com/savq/paq-nvim
-local paq_path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
-local paq_installed = vim.fn.empty(vim.fn.glob(path)) == 0
-if not paq_installed then
-  return vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
-end
-if paq_installed then
-  require "paq" {
-    "savq/paq-nvim",
-
-    -- plugins
-    "BurntSushi/ripgrep",
-    "LinArcX/telescope-command-palette.nvim",
-    "bakpakin/janet.vim",
-    "tpope/vim-unimpaired",
-    { "folke/which-key.nvim", run = function() require("which-key").setup({ }) end },
-
-    -- themes
-    "NLKNguyen/papercolor-theme",
-    "arcticicestudio/nord-vim",
-    "cocopon/iceberg.vim",
-    "preservim/vim-colors-pencil",
-    "ray-x/aurora",
-    "rebelot/kanagawa.nvim",
-    "sts10/vim-pink-moon",
-    "wadackel/vim-dogrun",
-    { "Mofiqul/dracula.nvim", as = "dracula" },
-    { "catppuccin/nvim", as = "catppuccin" },
-    { "challenger-deep-theme/vim", as = "challenger-deep-theme" },
-    { "embark-theme/vim", as = "embark-theme" },
-    "archseer/colibri.vim",
-  }
-end 
-
-if vim.g.goneovim then
-  -- https://github.com/akiyosi/goneovim
-  -- scoop install goneovim
-  -- :h goneovim.txt
-  vim.cmd("command! GonvimConfig :split ~/.goneovim/settings.toml")
-end
-
+-- https://neovide.dev/ `scoop install neovide`
 if vim.g.neovide then
-  -- https://neovide.dev/
-  -- scoop install neovide
 	vim.g.neovide_remember_window_size = true
   vim.g.neovide_floating_blur_amount_x = 2.0
   vim.g.neovide_floating_blur_amount_y = 2.0
@@ -111,7 +67,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0.13
   vim.g.neovide_cursor_trail_size = 0.8
   vim.g.neovide_cursor_unfocused_outline_width = 0.125
-  vim.g.neovide_cursor_vfx_mode = "" -- railgun, torpedo, pixiedust, sonicboom, ripple, wireframe
+  vim.g.neovide_cursor_vfx_mode = '' -- railgun, torpedo, pixiedust, sonicboom, ripple, wireframe
   vim.g.neovide_cursor_vfx_opacity = 200.0
   vim.g.neovide_cursor_vfx_particle_curl = 1.0 -- only for railgun
   vim.g.neovide_cursor_vfx_particle_density = 7.0
@@ -119,8 +75,89 @@ if vim.g.neovide then
   vim.g.neovide_cursor_vfx_particle_speed = 3.0
 
   vim.g.neovide_fullscreen = false
-  vim.keymap.set("", "<F11>", function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
+  vim.keymap.set('', '<F11>', function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
 end
 
--- Links
--- https://github.com/rockerBOO/awesome-neovim
+-- https://github.com/hismailbulut/Neoray
+if vim.g.neoray then
+	vim.cmd([[
+	set guifont=Go_Mono:h11
+	NeoraySet CursorAnimTime 0.08
+	NeoraySet Transparency   0.95
+	NeoraySet TargetTPS      120
+	NeoraySet BoxDrawing     true
+	NeoraySet ImageViewer    true
+	NeoraySet WindowSize     100x40
+	NeoraySet WindowState    centered
+	NeoraySet KeyFullscreen  <M-C-CR>
+	NeoraySet KeyZoomIn      <C-ScrollWheelUp>
+	NeoraySet KeyZoomOut     <C-ScrollWheelDown>
+
+	NeoraySet ContextMenu true
+	]])
+end
+
+-- https://github.com/yatli/fvim
+if vim.g.fvim_loaded then
+	vim.cmd([[
+	FVimCursorSmoothMove v:true
+	FVimCursorSmoothBlink v:true
+	]])
+end
+
+-- https://github.com/RMichelsen/Nvy
+if vim.g.nvy then
+end
+
+local packs = {
+  -- plugins
+  'BurntSushi/ripgrep',
+  'bakpakin/janet.vim',
+  'tpope/vim-unimpaired',
+  'gennaro-tedesco/nvim-peekup',
+  'nvim-lua/plenary.nvim',
+  'nvim-telescope/telescope.nvim',
+  'stevearc/dressing.nvim',
+  { 
+    'tomasky/bookmarks.nvim', 
+    run = function()
+      require('bookmarks').setup({
+        save_file = vim.fn.stdpath('data') .. '/bookmarks'
+      })
+    end
+  },
+  { 'folke/which-key.nvim', run = function() require('which-key').setup({ }) end },
+
+  -- themes
+  'NLKNguyen/papercolor-theme',
+  'archseer/colibri.vim',
+  'arcticicestudio/nord-vim',
+  'cocopon/iceberg.vim',
+  'preservim/vim-colors-pencil',
+  'ray-x/aurora',
+  'rebelot/kanagawa.nvim',
+  'sainnhe/sonokai',
+  'sts10/vim-pink-moon',
+  'wadackel/vim-dogrun',
+  { 'Mofiqul/dracula.nvim', as = 'dracula' },
+  { 'catppuccin/nvim', as = 'catppuccin' },
+  { 'challenger-deep-theme/vim', as = 'challenger-deep-theme' },
+  { 'embark-theme/vim', as = 'embark-theme' },
+}
+
+-- paq.nvim https://github.com/savq/paq-nvim
+local paq = require('paq')
+if true and package.loaded['paq'] then 
+  table.insert(packs, 1, 'savq/paq-nvim')
+  paq(packs)
+end
+
+-- lazy.nvim https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if false and not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({"git clone --filter=blob:none https://github.com/folke/lazy.nvim.git --branch=stable", lazypath})
+end
+if false and vim.loop.fs_stat(lazypath) then
+  vim.opt.rtp:prepend(lazypath)
+  require("lazy").setup(packs, opts)
+end
