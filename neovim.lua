0d94e19632646f4213d3391dba2176b81e7d00e4
Rafael Sanchez <rafael.sanchez.aguilu@gmail.com>
2024-09-24 16:16:19 -0400

    Updated to single file config.

--- neovim.lua
+++ neovim.lua
@@ -17,33 +17,105 @@ vim.api.nvim_create_autocmd("FileType", {
 
 -- Mappings
 
-vim.g.mapleader = " " -- leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 -- Plugins https://dotfyle.com/neovim/plugins/trending https://github.com/folke/lazy.nvim#-plugin-spec
 
-local packs = {}
+local packs = {
+  { 'echasnovski/mini.nvim', version = '*' }, -- Small neovim library/set of lua modules. :h mini.mvim
+  { 'epwalsh/obsidian.nvim', version = '*', lazy = true, ft = 'markdown', dependencies = 'nvim-lua/plenary.nvim' },
+  { 'gbprod/yanky.nvim' },
+  { 'ggandor/leap.nvim' },
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  { 'nvim-treesitter/nvim-treesitter' },
+  {
+    'stevearc/aerial.nvim', -- aerial, displays a code outline window
+    opts = {
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+      vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+  { 'tpope/vim-unimpaired' },
+  { 'nvim-tree/nvim-web-devicons' },
+}
 
--- Lazy https://lazy.folke.io/
+-- Themes
+local themes = {
+  { 'NLKNguyen/papercolor-theme' },
+  { 'Verf/deepwhite.nvim' },
+  { 'ajgrf/parchment' },
+  { 'archseer/colibri.vim' },
+  { 'axvr/raider.vim' },
+  { 'catppuccin/nvim', name = 'catppuccin_theme' },
+  { 'challenger-deep-theme/vim', name = 'challenger_deep_theme'},
+  { 'cocopon/iceberg.vim' },
+  { 'diegoulloao/neofusion.nvim' },
+  { 'dracula/vim', name = 'dracula_theme' },
+  { 'drazil100/dusklight.vim' },
+  { 'echasnovski/mini.base16' },
+  { 'embark-theme/vim', name = 'embark_theme' },
+  { 'folke/tokyonight.nvim' },
+  { 'jaredgorski/fogbell.vim' },
+  { 'josebalius/vim-light-chromeclipse' },
+  { 'kvrohit/mellow.nvim' },
+  { 'lmburns/kimbox' },
+  { 'mcchrish/zenbones.nvim' },
+  { 'morhetz/gruvbox' },
+  { 'nelstrom/vim-mac-classic-theme' },
+  { 'nordtheme/vim', name = 'nord_theme'},
+  { 'ntbbloodbath/sweetie.nvim' },
+  { 'nyoom-engineering/oxocarbon.nvim' },
+  { 'ok-ryoko/blue-screen' },
+  { 'olivercederborg/poimandres.nvim' },
+  { 'olivertaylor/vacme' },
+  { 'oxfist/night-owl.nvim' },
+  { 'preservim/vim-colors-pencil' },
+  { 'progrimorio/nvim-nyctophilia' },
+  { 'ray-x/aurora' },
+  { 'rebelot/kanagawa.nvim' },
+  { 'rose-pine/neovim', name = 'rosepine_theme' },
+  { 'sainnhe/everforest' },
+  { 'sainnhe/sonokai' },
+  { 'savq/melange-nvim' },
+  { 'slugbyte/lackluster.nvim' },
+  { 'sts10/vim-pink-moon' },
+  { 'wadackel/vim-dogrun' },
+}
+
+-- Lazy https://lazy.folke.io/installation
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-local lazyopts = {}
-
-if vim.fn.has("win32") == 1 then
-  lazyopts.browser = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
+vim.opt.rtp:prepend(lazypath)
 
 if not vim.g.lazy_did_setup then
-  vim.opt.rtp:prepend(lazypath)
-  --require("lazy").setup(packs, lazyopts)
-  require("lazy").setup("plugins", lazyopts)
+  -- https://lazy.folke.io/configuration
+  require("lazy").setup({
+    spec = { packs, themes },
+    install = { colorscheme = { "habamax" } },
+    checker = { enabled = true },
+  })
 end
